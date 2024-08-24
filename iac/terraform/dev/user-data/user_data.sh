771a063076c5b47277703b4adc167125b7d1e972
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2024-08-24 13:20:15 -0400

    wip

--- iac/terraform/dev/user-data/user_data.sh
+++ iac/terraform/dev/user-data/user_data.sh
@@ -1,6 +1,7 @@
 # installs git and jenkins
 sudo yum update –y
 sudo yum install git -y
+sudo yum install nodejs npm -y
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 sudo dnf install java-17-amazon-corretto -y
